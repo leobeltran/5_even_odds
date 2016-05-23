f7f830cc6f3d455c3264f75727053dce02f6f3f5
Leo Beltran <alpha059a@gmail.com>
2016-05-22 22:57:03 -0400

    first commit
    basic layout,

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+
+var numWins = 0;
+var numLosses = 0;
+
+var wins = documnet.getElementById("wins");
+var losses = document.getElementById("losses");
+var userHand = document.getElementById("user-choice");
+var compHand = document.getElementById("comp-choice");
+
